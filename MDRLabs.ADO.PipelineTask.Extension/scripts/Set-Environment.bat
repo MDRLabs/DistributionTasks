e9f267b784823c1be42544d010ea74409a624eba
Michael Bentley <michaelrbentley@outlook.com>
2021-06-02 16:11:23 +0200

    renamed variables

--- MDRLabs.ADO.PipelineTask.Extension/scripts/Set-Environment.bat
+++ MDRLabs.ADO.PipelineTask.Extension/scripts/Set-Environment.bat
@@ -1,6 +1,6 @@
-SET npm_config_ado_pat=your-pat-goes-here
-SET ADOAccount=your-devops-organisation-goes-here
-SET Publisher=MDRLabs-your-name-goes-here
-SET ADOExtensionDeveloper=your-name-goes-here
+SET npm_cfg_ado_pat=your-pat-goes-here
+SET npm_cfg_ado_org=your-devops-organisation-goes-here
+SET npm_cfg_ado_ext_publisher=MDRLabs-your-name-goes-here
+SET npm_cfg_ado_ext_developer=your-name-goes-here
 
 REM tfx extension publisher delete --publisher MyPublisherId
--- MDRLabs.ADO.PipelineTask.Extension/scripts/Set-Environment.sh
+++ MDRLabs.ADO.PipelineTask.Extension/scripts/Set-Environment.sh
@@ -1,6 +1,6 @@
-export npm_config_ado_pat=your-pat-goes-here
-export ADOAccount=MDRLabs
-export Publisher=MDRLabs-your-name-goes-here
-export ADOExtensionDeveloper=your-name-goes-here
+export npm_cfg_ado_pat=your-pat-goes-here
+export npm_cfg_ado_org=MDRLabs
+export npm_cfg_ado_ext_publisher=MDRLabs-your-name-goes-here
+export npm_cfg_ado_ext_developer=your-name-goes-here
 
 ## tfx extension publisher delete --publisher MyPublisherId
